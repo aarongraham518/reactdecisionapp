854f59655511386561d536d543bc0f063e880c6f
Aaron Graham <aarongraham518@gmail.com>
2020-09-17 22:45:26 -0400

    setup props within the Options cp and passed down to the Option cp

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 class IndecisionApp extends React.Component {
     render(){
+        const title = 'Indecision';
+        const subtitle = '!Put your life in the hands of a computer';
+        const options = ['thing one', 'thing two', 'thing four'];
+
         return (
             <div>
-                <Header />
+                <Header title={title} subtitle={subtitle}/>
                 <Action />
-                <Options />
+                <Options options={options}/>
                 <AddOption />
             </div>
         );
@@ -12,10 +16,11 @@ class IndecisionApp extends React.Component {
 }
 class Header extends React.Component {
     render() {
+        console.log(this.props);
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
@@ -35,8 +40,10 @@ class Options extends React.Component {
     render(){
         return(
             <div>
-                <p>Options component here</p>
-                <Option/>
+                {
+                    this.props.options.map((option) =>
+                     <Option key={option} optionText={option}/>)
+                } 
             </div>
         );
     }
@@ -45,7 +52,9 @@ class Options extends React.Component {
 class Option extends React.Component {
     render(){
         return(
-            <p>Option component here</p>
+            <div>
+                {this.props.optionText}
+            </div>
         );
     }
 }
