244756cce209b15bc17fa698e813cd5cf16f3199
Aaron Graham <aarongraham518@gmail.com>
2020-09-20 20:44:47 -0400

    added lifecyle hooks to counter app

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -12,7 +12,22 @@ class Counter extends React.Component{
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count
+            count: 0
+        }
+    }
+
+    componentDidMount(){
+        const stringCount = localStorage.getItem('count');
+        const count = parseInt(stringCount, 10);
+
+        if(!isNaN(count)){
+            this.setState(() => ({count: count}))
+        }
+    }
+    //save count when component changes
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.count !== this.state.count){
+            localStorage.setItem('count', this.state.count);
         }
     }
     handleAddOne(){
@@ -52,41 +67,4 @@ class Counter extends React.Component{
     }
 }
 
-Counter.defaultProps = {
-    count: 0
-};
-
 ReactDOM.render(<Counter />, document.getElementById('app'));
-//note, we can pass in count={-10} for default prop count.
-
-
-// let count = 0;
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-// };
-
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// }
-
-// const resetBtn = () => {
-//     count = 0;
-//     renderCounterApp();
-// }
-
-// //Our manual re-render function before we get to components
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={addOne}>+1</button>
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={resetBtn}>Reset</button>
-//         </div>
-//     );
-//     ReactDOM.render(templateTwo, appRoot); 
-// }
-
-// renderCounterApp();
