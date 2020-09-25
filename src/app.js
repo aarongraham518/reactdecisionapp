b9baa2690be4912a998d5fe2ca1279da0008b476
Aaron Graham <aarongraham518@gmail.com>
2020-09-24 23:06:41 -0400

    seperated components into individual files

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
-import './utils.js';
-console.log('app.js is running!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  
